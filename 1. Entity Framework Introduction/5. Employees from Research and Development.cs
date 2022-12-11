19939af271dd7bdb36edbf2b0aac3eb6ae2df989
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-12-12 00:00:16 +0200

    Update 5. Employees from Research and Development.cs

--- 1. Entity Framework Introduction/5. Employees from Research and Development.cs
+++ 1. Entity Framework Introduction/5. Employees from Research and Development.cs
@@ -1,40 +1,40 @@
-﻿//using System;
-//using System.Text;
-//using SoftUni.Models;
-//
-//namespace SoftUni
-//{
-//    public class StartUpExercise4
-//    {
-//        static void Main(string[] args)
-//        {
-//            var dbContext = new SoftUniContext();
-//            var result = GetEmployeesFromResearchAndDevelopment(dbContext);
-//            Console.WriteLine(result);
-//        }
-//
-//        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
-//        {
-//            var employeesFromRnD = context.Employees
-//                .OrderBy(x => x.Salary).ThenByDescending(x => x.FirstName)
-//                .Select(x => new
-//                {
-//                    x.FirstName,
-//                    x.LastName,
-//                    x.Department,
-//                    x.Salary
-//                })
-//                .Where(x => x.Department.DepartmentId == 6)
-//                .ToList();
-//
-//            var sb = new StringBuilder();
-//
-//            foreach (var employee in employeesFromRnD)
-//            {
-//                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.Department.Name} - ${employee.Salary:F2}");
-//            }
-//            var result = sb.ToString().TrimEnd();
-//            return result;
-//        }
-//    }
-//}
+using System;
+using System.Text;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUpExercise4
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+            var result = GetEmployeesFromResearchAndDevelopment(dbContext);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employeesFromRnD = context.Employees
+                .OrderBy(x => x.Salary).ThenByDescending(x => x.FirstName)
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.Department,
+                    x.Salary
+                })
+                .Where(x => x.Department.DepartmentId == 6)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employeesFromRnD)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.Department.Name} - ${employee.Salary:F2}");
+            }
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
