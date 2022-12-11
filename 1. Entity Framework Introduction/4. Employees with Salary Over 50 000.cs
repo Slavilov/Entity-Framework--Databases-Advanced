98b231a7b082bc97d2ac12179d71f1fa8e38c662
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-12-11 23:58:03 +0200

    Add files via upload

---
+++ 1. Entity Framework Introduction/4. Employees with Salary Over 50 000.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Text;
+//using SoftUni.Models;
+//
+//namespace SoftUni
+//{
+//    public class StartUpExercise3
+//    {
+//        static void Main(string[] args)
+//        {
+//            var db = new SoftUniContext();
+//            var result = GetEmployeesWithSalaryOver50000(db);
+//            Console.WriteLine(result);
+//        }
+//
+//        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+//        {
+//            var employeeSalaries = context.Employees
+//                .OrderBy(x => x.FirstName)
+//                .Select(x => new
+//                {
+//                    x.FirstName,
+//                    x.Salary
+//                })
+//                .Where(x => x.Salary > 50000)
+//                .ToList();
+//
+//            var sb = new StringBuilder();
+//
+//            foreach (var employee in employeeSalaries)
+//            {
+//                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+//            }
+//            var result = sb.ToString().TrimEnd();
+//
+//            return result;
+//        }
+//    }
+//}
