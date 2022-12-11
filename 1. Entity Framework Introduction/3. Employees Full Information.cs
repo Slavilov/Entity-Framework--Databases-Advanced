be9bd77969962b8b1c0647d66a268798606b0f48
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-12-11 23:59:17 +0200

    Update 3. Employees Full Information.cs

--- 1. Entity Framework Introduction/3. Employees Full Information.cs
+++ 1. Entity Framework Introduction/3. Employees Full Information.cs
@@ -1,39 +1,39 @@
-﻿// using System;
-//using System.Text;
-//using SoftUni.Models;
-//
-//namespace SoftUni
-//{
-//    public class StartUpExercise2
-//    {
-//        static void Main(string[] args)
-//        {
-//            var dbContext = new SoftUniContext();
-//            var result = GetEmployeesFullInformation(dbContext);
-//            Console.WriteLine(result);
-//        }
-//
-//        public static string GetEmployeesFullInformation(SoftUniContext context)
-//        {
-//            var employees = context.Employees
-//                .OrderBy(x => x.EmployeeId)
-//                .Select(x => new
-//                {
-//                    x.FirstName,
-//                    x.LastName,
-//                    x.MiddleName,
-//                    x.JobTitle,
-//                    x.Salary
-//                })
-//                .ToList();
-//            var sb = new StringBuilder();
-//
-//            foreach (var employee in employees)
-//            {
-//                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
-//            }
-//            var result = sb.ToString().TrimEnd();
-//            return result;
-//        }
-//    }
-//}
+using System;
+using System.Text;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUpExercise2
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+            var result = GetEmployeesFullInformation(dbContext);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .OrderBy(x => x.EmployeeId)
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.MiddleName,
+                    x.JobTitle,
+                    x.Salary
+                })
+                .ToList();
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
