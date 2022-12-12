59d0c15471da52b43e48ce3c6fe8df1af689e456
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-12-13 00:09:02 +0200

    Add files via upload

---
+++ 1. Entity Framework Introduction/7. Employees and Projects.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//using System.Globalization;
+//using System.Text;
+//using SoftUni_Second_Attempt_DB.Models;
+//
+//namespace SoftUni
+//{
+//    public class StartUp
+//    {
+//        static void Main(string[] args)
+//        {
+//            var context = new SoftUniContext();
+//
+//            var employees = context
+//                .Employees
+//                .Where(x => x.Projects.Any(ep => ep.StartDate.Year >= 2001 && ep.StartDate.Year <= 2003))
+//                .Select(e => new
+//                {
+//                    e.FirstName,
+//                    e.LastName,
+//                    ManagerFirstName = e.Manager.FirstName,
+//                    ManagerLastName = e.Manager.LastName,
+//                    Projects = e.Projects
+//                }).Take(10)
+//                .ToList();
+//
+//            foreach (var emp in employees)
+//            {
+//                Console.WriteLine($"{emp.FirstName} {emp.LastName} - Manager: {emp.ManagerFirstName} {emp.ManagerLastName}");
+//                foreach (var project in emp.Projects)
+//                {
+//                    string startDate = project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+//                    string endDate = project.EndDate != null
+//                        ? project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+//                        : "not finished";
+//
+//                    Console.WriteLine($"--{project.Name} - {startDate} - {endDate}");
+//                }
+//            }
+//        }
+//    }
+//}
