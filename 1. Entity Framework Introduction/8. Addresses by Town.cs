7a3c68559a2faec64d0934c376e443bc734c9734
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-12-13 00:14:29 +0200

    Update 8. Addresses by Town.cs

--- 1. Entity Framework Introduction/8. Addresses by Town.cs
+++ 1. Entity Framework Introduction/8. Addresses by Town.cs
@@ -1,42 +1,42 @@
-﻿//using System;
-//using System.Globalization;
-//using System.Text;
-//using SoftUni_Second_Attempt_DB.Models;
-//
-//namespace SoftUni
-//{
-//    public class StartUp
-//    {
-//        static void Main(string[] args)
-//        {
-//            var context = new SoftUniContext();
-//            var result = GetAddressesByTown(context);
-//            Console.WriteLine(result);
-//        }
-//        public static string GetAddressesByTown(SoftUniContext context)
-//        {
-//            var addresses = context.Addresses
-//                .OrderByDescending(x => x.Employees.Count())
-//                .ThenBy(x => x.Town.Name)
-//                .ThenBy(x => x.AddressText)
-//                .Select(x => new
-//                {
-//                    AddressText = x.AddressText,
-//                    TownName = x.Town.Name,
-//                    EmployeeCount = x.Employees.Count()
-//                })
-//                .Take(10)
-//                .ToList();
-//
-//            var sb = new StringBuilder();
-//
-//            foreach (var address in addresses)
-//            {
-//                sb.AppendLine($"{address.AddressText}, {address.TownName} - {address.EmployeeCount} employees");
-//            }
-//            var result = sb.ToString().TrimEnd();
-//            return result;
-//
-//        }
-//    }
-//}
+using System;
+using System.Globalization;
+using System.Text;
+using SoftUni_Second_Attempt_DB.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var result = GetAddressesByTown(context);
+            Console.WriteLine(result);
+        }
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var addresses = context.Addresses
+                .OrderByDescending(x => x.Employees.Count())
+                .ThenBy(x => x.Town.Name)
+                .ThenBy(x => x.AddressText)
+                .Select(x => new
+                {
+                    AddressText = x.AddressText,
+                    TownName = x.Town.Name,
+                    EmployeeCount = x.Employees.Count()
+                })
+                .Take(10)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var address in addresses)
+            {
+                sb.AppendLine($"{address.AddressText}, {address.TownName} - {address.EmployeeCount} employees");
+            }
+            var result = sb.ToString().TrimEnd();
+            return result;
+
+        }
+    }
+}
