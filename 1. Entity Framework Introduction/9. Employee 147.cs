9c970199973d2d9e9507b593505814badfb4df2a
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-12-15 23:54:54 +0200

    Update 9. Employee 147.cs

--- 1. Entity Framework Introduction/9. Employee 147.cs
+++ 1. Entity Framework Introduction/9. Employee 147.cs
@@ -1,42 +1,42 @@
-﻿using System;
+using System;
 using System.Globalization;
-//using System.Text;
-//using SoftUni_Second_Attempt_DB.Models;
-//
-//namespace SoftUni
-//{
-//    public class StartUp
-//    {
-//        static void Main(string[] args)
-//        {
-//            var context = new SoftUniContext();
-//            var result = GetEmployee147(context);
-//            Console.WriteLine(result);
-//        }
-//        public static string GetEmployee147(SoftUniContext context)
-//        {
-//            var employee147 = context.Employees
-//                .Where(x => x.EmployeeId == 147)
-//                .Select(x => new
-//                {
-//                    FirstName = x.FirstName,
-//                    LastName = x.LastName,
-//                    JobTitle = x.JobTitle,
-//                    Projects = x.Projects
-//                })
-//                .First();
-//
-//            var sb = new StringBuilder();
-//
-//            sb.AppendLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
-//
-//            foreach (var projectName in employee147.Projects.OrderBy(x => x.Name))
-//            {
-//                sb.AppendLine(projectName.Name);
-//            }
-//
-//            var result = sb.ToString().TrimEnd();
-//            return result;
-//        }
-//    }
-//}
+using System.Text;
+using SoftUni_Second_Attempt_DB.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var result = GetEmployee147(context);
+            Console.WriteLine(result);
+        }
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var employee147 = context.Employees
+                .Where(x => x.EmployeeId == 147)
+                .Select(x => new
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    JobTitle = x.JobTitle,
+                    Projects = x.Projects
+                })
+                .First();
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
+
+            foreach (var projectName in employee147.Projects.OrderBy(x => x.Name))
+            {
+                sb.AppendLine(projectName.Name);
+            }
+
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
