1838166377f7b126c8a9af69a4f5f0dc22e8f27a
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-10 23:29:54 +0200

    Add files via upload

---
+++ 3. DataBase LINQ/2. MusicHub - Albums Info & Songs Above Duration/MusicHub/SongsAboveDuration.cs
@@ -0,0 +1,67 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context =
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            var result = ExportSongsAboveDuration(context, 4);
+            Console.WriteLine(result);
+            //Test your solutions here
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            TimeSpan durationTs = TimeSpan.FromTicks(duration);
+
+            var songs = context.Songs
+                .ToList()
+                .Where(x => x.Duration.TotalSeconds > duration)
+                .Select(x => new
+                {
+                    Name = x.Name,
+                    PerformerFullName = x.SongPerformers
+                    .Select(x => x.Performer.FirstName + " " + x.Performer.LastName).FirstOrDefault(),
+                    WriterName = x.Writer.Name,
+                    AlbumProducer = x.Album.Producer.Name,
+                    Duration = x.Duration
+                })
+                .OrderBy(x => x.Name)
+                .OrderBy(x => x.WriterName)
+                .OrderBy(x => x.PerformerFullName)
+                .ToList();
+            
+            var sb = new StringBuilder();
+            int i = 0;
+
+            foreach (var song in songs)
+            {
+                i++;
+
+                sb.AppendLine($"-Song #{i}");
+                sb.AppendLine($"---SongName: {song.Name}");
+                sb.AppendLine($"---Writer: {song.WriterName}");
+                sb.AppendLine($"---Performer: {song.PerformerFullName}");
+                sb.AppendLine($"---AlbumProducer: {song.AlbumProducer}");
+                sb.AppendLine($"---Duration: {song.Duration}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
