abd2be82500c0ae32ebf6106aa54428589430608
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-10 23:28:03 +0200

    Delete 3. DataBase LINQ/02. Albums Info directory

--- 3. DataBase LINQ/02. Albums Info/Albums Info/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicHub.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString =
-            @"server=USER150407-1\\SQLEXPRESS;database=MusicHub#2;trusted_connection=true;";
-    }
-}
