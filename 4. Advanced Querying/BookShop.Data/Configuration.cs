5ceb1983aae14b97b6638e1206963d455764abbe
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-11 23:51:17 +0200

    Add files via upload

---
+++ 4. Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString 
+            => "server=USER150407-1\\SQLEXPRESS;database=BookShop;trusted_connection=true";
+    }
+}
---
+++ 4. Advanced Querying/BookShop.Models/Enums/AgeRestriction.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Models.Enums
+{
+    public enum AgeRestriction
+    {
+        minor = 0,
+        teen = 1,
+        adult = 2
+    }
+}
