248a9de9e101070926d5691481fda10305ed9137
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-12 22:36:14 +0200

    Add files via upload

---
+++ 4. Advanced Querying/BookShop/3.GoldenBooks.cs
@@ -0,0 +1,45 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+            DbInitializer.Seed(context);
+
+            var result = GetGoldenBooks(context);
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldenBooksTitles = context.Books
+                .Where(x => x.EditionType == EditionType.Gold && x.Copies < 5000)
+                .Select(x => new
+                {
+                    BookId = x.BookId,
+                    BookTitle = x.Title
+                })
+                .OrderBy(x => x.BookId)
+                .ToList();
+
+            var sb = new StringBuilder();
+            
+            foreach (var title in goldenBooksTitles)
+            {
+                sb.AppendLine(title.BookTitle);
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
