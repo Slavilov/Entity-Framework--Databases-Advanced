8e1d93eed23f883ee943452ce4b2d7c93f62c7aa
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-12 22:36:44 +0200

    Add files via upload

---
+++ 4. Advanced Querying/BookShop/4.BooksByPrice.cs
@@ -0,0 +1,46 @@
+﻿//namespace BookShop
+//{
+//    using BookShop.Models.Enums;
+//    using Data;
+//    using Initializer;
+//    using System;
+//    using System.Linq;
+//    using System.Text;
+//
+//    public class StartUp
+//    {
+//        public static void Main()
+//        {
+//            using var context = new BookShopContext();
+//
+//            DbInitializer.ResetDatabase(context);
+//            DbInitializer.Seed(context);
+//
+//            var result = GetBooksByPrice(context);
+//            Console.WriteLine(result);
+//
+//        }
+//
+//        public static string GetBooksByPrice(BookShopContext context)
+//        {
+//            var titlesAndPricesOfBooks = context.Books
+//                .Where(x => x.Price > 40)
+//                .Select(x => new
+//                {
+//                    Title = x.Title,
+//                    Price = x.Price
+//                })
+//                .OrderByDescending(x => x.Price)
+//                .ToList();
+//
+//            var sb = new StringBuilder();
+//
+//            foreach (var book in titlesAndPricesOfBooks)
+//            {
+//                sb.AppendLine($"{book.Title} - ${book.Price:F2}");
+//            }
+//
+//            return sb.ToString();
+//        }
+//    }
+//}
