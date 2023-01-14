69c7618deeb4297264fe0c34c7d17311080122c8
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-14 22:51:20 +0200

    Add files via upload

---
+++ 4. Advanced Querying/BookShop/6.BookTitlesByCategory.cs
@@ -0,0 +1,52 @@
+﻿//namespace BookShop
+//{
+//    using BookShop.Models.Enums;
+//    using Data;
+//    using Initializer;
+//    using System;
+//    using System.Linq;
+//    using System.Text;
+//
+//    public class StartUp
+//    {
+//        public static void Main()
+//        {
+//            using var context = new BookShopContext();
+//
+//            //DbInitializer.ResetDatabase(context);
+//            //DbInitializer.Seed(context);
+//
+//            string inputCategories = Console.ReadLine();
+//            var result = GetBooksByCategory(context, inputCategories);
+//            Console.WriteLine(result);
+//
+//        }
+//
+//        public static string GetBooksByCategory(BookShopContext context, string inputCategories)
+//        {
+//            string[] Categories = inputCategories
+//                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+//                .ToArray();
+//
+//            var titleOfBooks = context.Books
+//                .Where(book => book.BookCategories
+//                   .Any(category => Categories.Contains
+//                   (category.Category.Name)))
+//                .Select(x=> new
+//                {
+//                    Title = x.Title
+//                })
+//                .OrderBy(x => x.Title)
+//                .ToList();
+//
+//            var sb = new StringBuilder();
+//
+//            foreach (var book in titleOfBooks)
+//            {
+//                sb.AppendLine($"A {book.Title}");
+//            }
+//
+//            return sb.ToString();
+//        }
+//    }
+//}
