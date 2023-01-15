91fe86e3a4a6b119991d7d7c33dc6c3ee9d01105
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-15 23:51:25 +0200

    Add files via upload

---
+++ 4. Advanced Querying/BookShop/7.ReleasedBeforeDate.cs
@@ -0,0 +1,52 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            //DbInitializer.ResetDatabase(context);
+            //DbInitializer.Seed(context);
+
+            string date = Console.ReadLine();
+            var result = GetBooksReleasedBefore(context, date);
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            DateTime dt = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var books = context.Books
+                .Where(x => x.ReleaseDate < dt)
+                .Select(x => new
+                {
+                    Title = x.Title,
+                    EditionType = x.EditionType,
+                    Price = x.Price,
+                    ReleaseDate = x.ReleaseDate
+                })
+                .OrderByDescending(x => x.ReleaseDate)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - {book.EditionType} - {book.Price}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
