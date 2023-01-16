8392ab1a78929a87e41915c4b19c7202dacdca39
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-16 23:00:11 +0200

    Update 9.BookSearch.cs

--- 4. Advanced Querying/BookShop/9.BookSearch.cs
+++ 4. Advanced Querying/BookShop/9.BookSearch.cs
@@ -1,48 +1,48 @@
-﻿//namespace BookShop
-//{
-//    using BookShop.Models.Enums;
-//    using Data;
-//    using Initializer;
-//    using System;
-//    using System.Globalization;
-//    using System.Linq;
-//    using System.Text;
-//
-//    public class StartUp
-//    {
-//        public static void Main()
-//        {
-//            using var context = new BookShopContext();
-//
-//            //DbInitializer.ResetDatabase(context);
-//            //DbInitializer.Seed(context);
-//
-//            string input = Console.ReadLine();
-//            var result = GetBookTitlesContaining(context, input);
-//            Console.WriteLine(result);
-//
-//        }
-//
-//        public static string GetBookTitlesContaining(BookShopContext context, string input)
-//        {
-//            var titlesOfBooks = context.Books
-//                .Where(x => x.Title.ToLower().Contains(input.ToLower()))
-//                .Select(x => new
-//                {
-//                    TitleName = x.Title
-//                })
-//                .OrderBy(x => x.TitleName)
-//                .ToList();
-//
-//
-//            var sb = new StringBuilder();
-//
-//            foreach (var book in titlesOfBooks)
-//            {
-//                sb.AppendLine($"{book.TitleName}");
-//            }
-//
-//            return sb.ToString();
-//        }
-//    }
-//}
+namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            //DbInitializer.ResetDatabase(context);
+            //DbInitializer.Seed(context);
+
+            string input = Console.ReadLine();
+            var result = GetBookTitlesContaining(context, input);
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var titlesOfBooks = context.Books
+                .Where(x => x.Title.ToLower().Contains(input.ToLower()))
+                .Select(x => new
+                {
+                    TitleName = x.Title
+                })
+                .OrderBy(x => x.TitleName)
+                .ToList();
+
+
+            var sb = new StringBuilder();
+
+            foreach (var book in titlesOfBooks)
+            {
+                sb.AppendLine($"{book.TitleName}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
