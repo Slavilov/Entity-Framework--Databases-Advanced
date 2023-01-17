8fa796066a32a45565b4e66bf8406a90f23b573a
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-17 22:55:41 +0200

    Add files via upload

---
+++ 4. Advanced Querying/BookShop/11. Count Books.cs
@@ -0,0 +1,35 @@
+﻿//namespace BookShop
+//{
+//    using BookShop.Models.Enums;
+//    using Data;
+//    using Initializer;
+//    using System;
+//    using System.Globalization;
+//    using System.Linq;
+//    using System.Text;
+//
+//    public class StartUp
+//    {
+//        public static void Main()
+//        {
+//            using var context = new BookShopContext();
+//
+//            //DbInitializer.ResetDatabase(context);
+//            //DbInitializer.Seed(context);
+//
+//            int lengthCheck = int.Parse(Console.ReadLine());
+//            var count = CountBooks(context, lengthCheck);
+//
+//            Console.WriteLine($"There are {count} books with longer title than {lengthCheck} symbols");
+//        }
+//
+//        public static int CountBooks(BookShopContext context, int lengthCheck)
+//        {
+//            var count = context.Books
+//                .Where(x=>x.Title.Length > lengthCheck)
+//                .Count();
+//
+//            return count;
+//        }
+//    }
+//}
