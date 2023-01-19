668a19949bced40a10ef3be4103c710634dcd8e1
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-19 22:43:50 +0200

    Update 12. Total Book Copies.cs

--- 4. Advanced Querying/BookShop/12. Total Book Copies.cs
+++ 4. Advanced Querying/BookShop/12. Total Book Copies.cs
@@ -1,46 +1,46 @@
-﻿//namespace BookShop
-//{
-//    using BookShop.Models.Enums;
-//    using Data;
-//    using Initializer;
-//    using System;
-//    using System.Globalization;
-//    using System.Linq;
-//    using System.Text;
-//
-//    public class StartUp
-//    {
-//        public static void Main()
-//        {
-//            using var context = new BookShopContext();
-//
-//            //DbInitializer.ResetDatabase(context);
-//            //DbInitializer.Seed(context);
-//
-//            var result = CountCopiesByAuthor(context);
-//            Console.WriteLine(result);
-//
-//        }
-//
-//        public static string CountCopiesByAuthor(BookShopContext context)
-//        {
-//            var authorQuantityOfBooks = context.Authors
-//                .Select(x => new
-//                {
-//                    AuthorName = x.FirstName + " " + x.LastName,
-//                    CountOfBooks = x.Books.Sum(x=>x.Copies)
-//                })
-//                .OrderByDescending(x => x.CountOfBooks)
-//                .ToList();
-//
-//            var sb = new StringBuilder();
-//
-//            foreach (var author in authorQuantityOfBooks)
-//            {
-//                sb.AppendLine($"{author.AuthorName} - {author.CountOfBooks}");
-//            }
-//
-//            return sb.ToString();
-//        }
-//    }
-//}
+namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            //DbInitializer.ResetDatabase(context);
+            //DbInitializer.Seed(context);
+
+            var result = CountCopiesByAuthor(context);
+            Console.WriteLine(result);
+
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var authorQuantityOfBooks = context.Authors
+                .Select(x => new
+                {
+                    AuthorName = x.FirstName + " " + x.LastName,
+                    CountOfBooks = x.Books.Sum(x=>x.Copies)
+                })
+                .OrderByDescending(x => x.CountOfBooks)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var author in authorQuantityOfBooks)
+            {
+                sb.AppendLine($"{author.AuthorName} - {author.CountOfBooks}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
