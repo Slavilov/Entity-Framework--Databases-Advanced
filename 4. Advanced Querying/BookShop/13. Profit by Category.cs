096269c704addc5458bc3cfc11f90330548e1227
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-20 23:09:16 +0200

    Update 13. Profit by Category.cs

--- 4. Advanced Querying/BookShop/13. Profit by Category.cs
+++ 4. Advanced Querying/BookShop/13. Profit by Category.cs
@@ -1,47 +1,47 @@
-﻿//namespace BookShop
-//{
-//    using BookShop.Models.Enums;
-//    using Data;
-//    using Initializer;
-//    using System;
-//    using System.Globalization;
-//    using System.Linq;
-//    using System.Text;
-//
-//    public class StartUp
-//    {
-//        public static void Main()
-//        {
-//            using var context = new BookShopContext();
-//
-//            //DbInitializer.ResetDatabase(context);
-//            //DbInitializer.Seed(context);
-//
-//            var result = GetTotalProfitByCategory(context);
-//            Console.WriteLine(result);
-//
-//        }
-//
-//        public static string GetTotalProfitByCategory(BookShopContext context)
-//        {
-//            var profitByCategory = context.Categories
-//                .Select(x => new
-//                {
-//                    CategoryName = x.Name,
-//                    Profit = x.CategoryBooks.Sum(x => x.Book.Copies * x.Book.Price)
-//                })
-//                .OrderByDescending(x => x.Profit)
-//                .ThenBy(x => x.CategoryName)
-//                .ToList();
-//
-//            var sb = new StringBuilder();
-//
-//            foreach (var category in profitByCategory)
-//            {
-//                sb.AppendLine($"{category.CategoryName} - ${category.Profit:F2}");
-//            }
-//
-//            return sb.ToString();
-//        }
-//    }
-//}
+namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            //DbInitializer.ResetDatabase(context);
+            //DbInitializer.Seed(context);
+
+            var result = GetTotalProfitByCategory(context);
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var profitByCategory = context.Categories
+                .Select(x => new
+                {
+                    CategoryName = x.Name,
+                    Profit = x.CategoryBooks.Sum(x => x.Book.Copies * x.Book.Price)
+                })
+                .OrderByDescending(x => x.Profit)
+                .ThenBy(x => x.CategoryName)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var category in profitByCategory)
+            {
+                sb.AppendLine($"{category.CategoryName} - ${category.Profit:F2}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
