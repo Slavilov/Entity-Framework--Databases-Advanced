2e0afbd361df3b18615b56d05fc010d0afea603c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-22 23:40:02 +0200

    Update 15. Increase Prices.cs

--- 4. Advanced Querying/BookShop/15. Increase Prices.cs
+++ 4. Advanced Querying/BookShop/15. Increase Prices.cs
@@ -21,35 +21,18 @@
             Console.WriteLine(result);
 
         }
-
-        static string GetMostRecentBooks(BookShopContext context)
+        
+         static int RemoveBooks(BookShopContext dbContext)
         {
+            Book[] books = dbContext
+                .Books
+                .Where(b => b.Copies < 4200)
+                .ToArray();
 
-            var top3RecentBooksByCategory = context.Categories
-                .Select(x => new
-                {
-                    CategoryName = x.Name,
-                    Top3Books = x.CategoryBooks
-                    .OrderByDescending(x => x.Book.ReleaseDate)
-                    .Select(x => x.Book).Take(3)
-                })
-                .OrderBy(x => x.CategoryName)
-                .ToList();
+            dbContext.Books.RemoveRange(books);
+            dbContext.SaveChanges();
 
-            var sb = new StringBuilder();
-
-            foreach (var category in top3RecentBooksByCategory)
-            {
-                sb.AppendLine($"--{category.CategoryName}");
-
-                foreach (var book in category.Top3Books)
-                {
-                    sb.AppendLine($"{book.Title} ({book.ReleaseDate.Value.Year})");
-                }
-                sb.AppendLine($" ");
-            }
-
-            return sb.ToString();
+            return books.Length;
         }
     }
 }
