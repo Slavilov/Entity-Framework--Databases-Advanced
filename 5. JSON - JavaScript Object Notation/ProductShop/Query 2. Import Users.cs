68155fed65619f8a37143446aa5f62da31c88518
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-25 22:57:52 +0200

    Update Query 2. Import Users.cs

--- 5. JSON - JavaScript Object Notation/ProductShop/Query 2. Import Users.cs
+++ 5. JSON - JavaScript Object Notation/ProductShop/Query 2. Import Users.cs
@@ -1,39 +1,39 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using Newtonsoft.Json;
-//using ProductShop.Data;
-//using ProductShop.Models;
-//
-//namespace ProductShop
-//{
-//    public class StartUp
-//    {
-//        public static void Main(string[] args)
-//        {
-//            var db = new ProductShopContext();
-//            db.Database.EnsureCreated();
-//
-//            string jsonString = File.ReadAllText(@"../../../Datasets/users.json");
-//
-//            var result = ImportUsers(db, jsonString);
-//            Console.WriteLine(result);
-//
-//        }
-//        public static string ImportUsers(ProductShopContext context, string inputJson)
-//        {
-//            var usersFromJson = JsonConvert.DeserializeObject<List<User>>(inputJson);
-//
-//            foreach (var user in usersFromJson)
-//            {
-//                context.Users.Add(user);
-//            }
-//            context.SaveChanges();
-//
-//            string result = $"Successfully imported {usersFromJson.Count}";
-//
-//            return result;
-//        }
-//    }
-//}
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var db = new ProductShopContext();
+            db.Database.EnsureCreated();
+
+            string jsonString = File.ReadAllText(@"../../../Datasets/users.json");
+
+            var result = ImportUsers(db, jsonString);
+            Console.WriteLine(result);
+
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var usersFromJson = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            foreach (var user in usersFromJson)
+            {
+                context.Users.Add(user);
+            }
+            context.SaveChanges();
+
+            string result = $"Successfully imported {usersFromJson.Count}";
+
+            return result;
+        }
+    }
+}
