99449cd13eb1b836bc34aee8c38b0083890fe82c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-25 22:58:21 +0200

    Add files via upload

---
+++ 5. JSON - JavaScript Object Notation/ProductShop/Query 3. Import Products.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using Newtonsoft.Json;
+//using ProductShop.Data;
+//using ProductShop.Models;
+//using AutoMapper;
+//
+//namespace ProductShop
+//{
+//    public class StartUp
+//    {
+//        static IMapper mapper;
+//
+//        public static void Main(string[] args)
+//        {
+//            var db = new ProductShopContext();
+//            db.Database.EnsureCreated();
+//
+//            string jsonString = File.ReadAllText(@"../../../Datasets/products.json");
+//
+//            var config = new MapperConfiguration(cfg =>
+//            {
+//                cfg.AddProfile<ProductShopProfile>();
+//            });
+//            mapper = config.CreateMapper();
+//
+//
+//            var result = ImportProducts(db, jsonString);
+//            Console.WriteLine(result);
+//
+//        }
+//        public static string ImportProducts(ProductShopContext context, string inputJson)
+//        {
+//            var DTOproductsFromJson = JsonConvert.DeserializeObject<List<ProductsInputModel>>(inputJson);
+//            var products = mapper.Map<List<Product>>(DTOproductsFromJson);
+//
+//            foreach (var product in products)
+//            {
+//                context.Products.Add(product);
+//            }
+//            context.SaveChanges();
+//
+//            string result = $"Successfully imported {context.Products.Count()}";
+//
+//            return result;
+//        }
+//    }
+//}
