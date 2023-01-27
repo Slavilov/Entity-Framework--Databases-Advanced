e84d3419629c87bb2987838fabdacfd01544ea67
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-27 23:52:57 +0200

    Update Query 4. Import Categories.cs

--- 5. JSON - JavaScript Object Notation/ProductShop/Query 4. Import Categories.cs
+++ 5. JSON - JavaScript Object Notation/ProductShop/Query 4. Import Categories.cs
@@ -1,54 +1,54 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using Newtonsoft.Json;
-//using ProductShop.Data;
-//using ProductShop.Models;
-//using AutoMapper;
-//
-//namespace ProductShop
-//{
-//    public class StartUp
-//    {
-//        static IMapper mapper;
-//
-//        public static void Main(string[] args)
-//        {
-//            var db = new ProductShopContext();
-//            db.Database.EnsureCreated();
-//
-//            string jsonString = File.ReadAllText(@"../../../Datasets/categories.json");
-//
-//            var config = new MapperConfiguration(cfg =>
-//            {
-//                cfg.AddProfile<ProductShopProfile>();
-//            });
-//            mapper = config.CreateMapper();
-//
-//
-//            var result = ImportProducts(db, jsonString);
-//            Console.WriteLine(result);
-//
-//        }
-//        static string ImportProducts(ProductShopContext context, string inputJson)
-//        {
-//            var DTOcategoriesFromJson = JsonConvert.DeserializeObject<List<CategoryInputModel>>(inputJson);
-//            var categories = mapper.Map<List<Category>>(DTOcategoriesFromJson);
-//
-//            foreach (var category in categories)
-//            {
-//                if (category.Name == null)
-//                {
-//                    continue;
-//                }
-//                context.Categories.Add(category);
-//            }
-//            context.SaveChanges();
-//
-//            string result = $"Successfully imported {context.Categories.Count()}";
-//
-//            return result;
-//        }
-//    }
-//}
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+using AutoMapper;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            var db = new ProductShopContext();
+            db.Database.EnsureCreated();
+
+            string jsonString = File.ReadAllText(@"../../../Datasets/categories.json");
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+            mapper = config.CreateMapper();
+
+
+            var result = ImportProducts(db, jsonString);
+            Console.WriteLine(result);
+
+        }
+        static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var DTOcategoriesFromJson = JsonConvert.DeserializeObject<List<CategoryInputModel>>(inputJson);
+            var categories = mapper.Map<List<Category>>(DTOcategoriesFromJson);
+
+            foreach (var category in categories)
+            {
+                if (category.Name == null)
+                {
+                    continue;
+                }
+                context.Categories.Add(category);
+            }
+            context.SaveChanges();
+
+            string result = $"Successfully imported {context.Categories.Count()}";
+
+            return result;
+        }
+    }
+}
