b7abb1f9e8fbe76b9939ee8e8fba6513486b728d
Velichko Slavilov <velichkoslavilov98@gmail.com>
2023-01-28 23:31:44 +0200

    Add files via upload

---
+++ 5. JSON - JavaScript Object Notation/ProductShop/Query 6. Query And Export Data.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using Newtonsoft.Json;
+//using ProductShop.Data;
+//using ProductShop.Models;
+//using AutoMapper;
+//
+//
+//namespace ProductShop
+//{
+//    public class StartUp
+//    {
+//        static IMapper mapper;
+//
+//        public static void Main(string[] args)
+//        {
+//            var db = new ProductShopContext();
+//            db.Database.EnsureCreated();
+//
+//            var result = GetProductsInRange(db);
+//            Console.WriteLine(result);
+//
+//        }
+//        public static string GetProductsInRange(ProductShopContext context)
+//        {
+//            var products = context.Products
+//                .Where(x => x.Price >= 500 && x.Price <= 1000)
+//                .Select(x => new
+//                {
+//                    ProductName = x.Name,
+//                    Price = x.Price,
+//                    SellerFullName = x.Seller.FirstName + " " + x.Seller.LastName
+//                })
+//                .OrderBy(x => x.Price)
+//                .ToList();
+//
+//            string json = JsonConvert.SerializeObject(products, Formatting.Indented);
+//
+//            return json;
+//        }
+//    }
+//}
